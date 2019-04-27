8332e38eb431fe3c6afcf4631d3fdff156a05913
Mike Dunn <mdunn@safaribooksonline.com>
2019-04-27 02:33:56 -0500

    wip

--- demo/src/main/java/com/moagrius/demo/MainActivity.java
+++ demo/src/main/java/com/moagrius/demo/MainActivity.java
@@ -10,11 +10,6 @@ public class MainActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    findViewById(R.id.textview_demos_scrollview_vertical).setOnClickListener(view -> startDemo(ScrollViewDemoVertical.class));
-    findViewById(R.id.textview_demos_scrollview_horizontal).setOnClickListener(view -> startDemo(ScrollViewDemoHorizontal.class));
-    findViewById(R.id.textview_demos_scrollview_universal).setOnClickListener(view -> startDemo(ScrollViewDemoUniversal.class));
-    findViewById(R.id.textview_demos_scalingscrollview_textviews).setOnClickListener(view -> startDemo(ScalingScrollViewDemoTextViews.class));
-    findViewById(R.id.textview_demos_scalingscrollview_tiger).setOnClickListener(view -> startDemo(ScalingScrollViewDemoTiger.class));
     findViewById(R.id.textview_demos_tileview_advanced).setOnClickListener(view -> startDemo(TileViewDemoAdvanced.class));
     findViewById(R.id.textview_demos_tileview_simple).setOnClickListener(view -> startDemo(TileViewDemoSimple.class));
   }
